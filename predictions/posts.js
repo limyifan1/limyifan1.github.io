ee21d3b436d3b86b2d57d1b521bc1da46c6084cb
Lim Yi Fan <35542285+limyifan1@users.noreply.github.com>
2025-08-13 02:34:30 +0800

    Update posts.js

--- predictions/posts.js
+++ predictions/posts.js
@@ -1,7 +1,7 @@
 const posts = [
   {
     file: "donald_trump_2025_south_korea.md",
-    title: "Will Dondla Trump visit South Korea in 2025",
+    title: "Will Donald Trump visit South Korea in 2025",
     date: "2025-08-13",
   },
 ];
